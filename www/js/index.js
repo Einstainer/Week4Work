0692f06ee9035185c0dc8ee0d6c4c377898c987a
Einstainer <rana2_15@uni.worc.ac.uk>
2017-10-16 15:07:08 +0100

    Fix iit
    better way of naming and calling code value before setting it

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
 var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
-var click_count = window.localStorage.getItem("click_count");
+var click_count = 0;
 
 document.addEventListener("deviceready", onDeviceReady, false);
 		
@@ -34,8 +34,9 @@ function updateDisplay() {
     
     function clickMe()
     {
+        click_count = window.localStorage.getItem("clicker")
         click_count++;
-        windows.localStorage.setItem("click_count", click_count);
+        windows.localStorage.setItem("clicker", click_count);
     }
 
 
